bb3e07bc4b823c5f3abc4e81109969085a38f332
Pduc <anhducngusi@gmail.com>
2022-02-14 16:19:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+
+import { getPlacesData } from "./api";
+
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [childClicked, setChildClicked] = useState(null);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+  useEffect(() => {
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+  useEffect(() => {
+    setIsLoading(true);
+    if (bounds) {
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data);
+        setFilteredPlaces([])
+        setIsLoading(false);
+      });
+    }
+  }, [coordinates, bounds, type]);
+  return (
+    <>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates}/>
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default App;
